eb6519bb1e04bf1e9ca0701788e46dd1d88c943c
Ndinayo <ndinayoeric1@gmail.com>
2025-06-25 13:48:56 +0200

    added maps functions

---
+++ 8 kyu/maps.js
@@ -0,0 +1,12 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+  return x.map(b => b*2)
+
+}
+
+console.log(maps([2,4,5]))
