01e8e03df0b2a537bf491d6e211bda35ace90e07
Ndinayo <ndinayoeric1@gmail.com>
2025-08-06 15:06:21 +0200

    7 kyu/6 kyu.js

---
+++ 7 kyu/6 kyu.js
@@ -0,0 +1,29 @@
+/*Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+For example, decode("h3 th2r2") would return "hi there".
+
+For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.*/
+
+function encode(string) {
+  let encodeObj={a:1, e:2, i:3, o:4, u:5}
+  const char = string.split('')
+  const encoded = char.map(char => encodeObj[char] || char).join("")
+  return encoded
+}
+
+function decode(string) {
+    let encodeObj = {1: "a", 2: "e", 3:"i", 4:"o", 5:"u"}
+    let char = string.split("")
+    const decoded = char.map(char=> encodeObj[char] || char).join('')
+    
+  return decoded
+}
